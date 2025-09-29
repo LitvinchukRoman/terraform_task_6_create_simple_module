66e366d8e91815edbc96e7b138ae9493d7c48b55
litvi <litvin4uk.roman@gmail.com>
2025-09-29 12:20:26 +0300

    Terraform version

--- modules/resource_group_storage/main.tf
+++ modules/resource_group_storage/main.tf
@@ -1,3 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+
 module "resource-group" {
   source  = "LitvinchukRoman/resource-group/azurerm"
   version = "1.0.1"
