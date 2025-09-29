24efff5941cd4d7ce3fed605b8a1194c3087fa9f
litvi <litvin4uk.roman@gmail.com>
2025-09-29 12:51:16 +0300

    Terraform version

--- modules/resource_group_storage/main.tf
+++ modules/resource_group_storage/main.tf
@@ -16,6 +16,7 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
+
 resource "azurerm_storage_account" "example" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.example.name
